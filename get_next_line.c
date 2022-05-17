eb054d03f1ef65de41acabe80c10cf8a8061c9d7
crist6494 <moralesrojascr@gmail.com.com>
2022-05-17 20:22:09 +0200

    Get_Next_Line

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 19:22:35 by cmorales          #+#    #+#             */
-/*   Updated: 2022/05/16 20:00:16 by cmorales         ###   ########.fr       */
+/*   Updated: 2022/05/17 20:16:45 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_readline(int	fd, char	*buffer)
+static char	*ft_readline(int fd, char *buff)
 {
 	int		read_pos;
 	char	*read_buffer;
@@ -23,18 +23,79 @@ static char	*ft_readline(int	fd, char	*buffer)
 		free(read_buffer);
 		return (0);
 	}
-	read_pos = 0;
-	while (!ft_strchr(buffer, '\n') && read_pos != 0)
+	read_pos = 1;
+	while (!ft_strchr(buff, '\n') && read_pos != 0)
 	{
 		read_pos = read(fd, read_buffer, BUFFER_SIZE);
-		if (read_pos < 0)
+		if (read_pos == -1)
 		{
 			free (read_buffer);
 			return (0);
 		}
 		read_buffer[read_pos] = '\0';
-		buffer = ft_strjoin(buffer, read_buffer);
+		buff = ft_strjoin(buff, read_buffer);
 	}
 	free(read_buffer);
-	return (buffer);	
+	return (buff);	
 }
+
+static char	*ft_get_line(char *buffer)
+{
+	int		pos;
+	char	*line;
+
+	pos = 0;
+	if (!buffer)
+		return (0);
+	while (buffer[pos] && buffer[pos] != '\n')
+		pos++;
+	line = ft_calloc(pos + 2, sizeof(char));
+	pos = 0;
+	while (buffer[pos] && buffer[pos] != '\n')
+	{
+		line[pos] = buffer[pos];
+		pos++;
+	}
+	if (buffer[pos] == '\n')
+		line[pos++] = '\n';
+	return (line);
+}
+
+static char	*ft_remove_line(char *buffer)
+{
+	int		pos;
+	char	*aux;
+	
+	pos = 0;
+	while (buffer[pos] && buffer[pos] != '\n')
+		pos++;
+	if (!buffer)
+		return (0);
+	aux = ft_substr(buffer, pos + 1, ft_strlen(buffer));
+	free(buffer);
+	return (aux);
+}
+
+char *get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+	
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+		return (0);
+	buffer = ft_readline(fd, buffer);
+	if (!buffer)
+		return (0);
+	line = ft_get_line(buffer);
+	line = ft_remove_line(buffer);
+	return (line);
+}
+
+/* int	main()
+{
+	int	fd;
+	
+	fd = open("pepe.txt", O_RDONLY);
+	get_next_line(fd);
+	return (0);
+} */
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,29 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 17:11:42 by cmorales          #+#    #+#             */
-/*   Updated: 2022/05/16 19:57:59 by cmorales         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:51:31 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#endif
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 500
+# define BUFFER_SIZE 42
 #endif
 
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
 void	*ft_calloc(size_t	c, size_t	n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char	*str);
+char 	*get_next_line(int fd);
+void	ft_bzero(void *s, size_t n);
+
+#endif
